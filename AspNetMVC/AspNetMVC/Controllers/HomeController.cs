1d75d10938ab1134cff3e11df8c213f6e53700b4
Duvan <duvan4448@gmail.com>
2020-05-20 23:16:09 -0500

    1-MiPrimeraAplicacion

---
+++ AspNetMVC/AspNetMVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Mi primera aplicación en ASP.NET MVC 5";
+        }
+    }
+}
