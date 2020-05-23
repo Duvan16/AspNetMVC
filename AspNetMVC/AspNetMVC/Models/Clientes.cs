e562c02724ca30775785b39e7945d4057c7dabb2
Duvan <duvan4448@gmail.com>
2020-05-23 00:12:30 -0500

    13-ValidacionDatos

--- AspNetMVC/AspNetMVC/Models/Clientes.cs
+++ AspNetMVC/AspNetMVC/Models/Clientes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -9,18 +10,26 @@ namespace AspNetMVC.Models
     public class Clientes
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string nombre { get; set; }
+
+        [Display(Name = "Fecha de Alta")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:YYYY-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime FechaAlta { get; set; }
+
+        [Range(18, 75)]
         public int edad { get; set; }
 
-        public class EmpDBContext: DbContext
+        public class EmpDBContext : DbContext
         {
             public EmpDBContext()
             {
 
             }
 
-            public  DbSet<Clientes> Clientes { get; set; }
+            public DbSet<Clientes> Clientes { get; set; }
         }
 
     }
